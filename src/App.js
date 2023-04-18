ba227a6fb21c71d0b4b63bb98878c107baea2dc5
BabyGuma <zS20020316@estudiantes.uv.mx>
2023-04-17 20:02:28 -0600

    Update #4

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
+import imageRickMorty from './img/rick-morty.png';
 import './App.css';
+import { useState } from 'react';
+import Characters from './components/Characters';
 
 function App() {
+  const [characters, setCharacters] = useState(null);
+
+  const reqApi = async () => {
+    const api = await fetch("https://rickandmortyapi.com/api/character");
+    const characterApi = await api.json();
+
+    setCharacters(characterApi.results);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Baby Guma
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn 
-        </a>
+        <h1 className="title">Rick & Morty</h1>
+        <h1 className="description">No me salio la pinche aplicacion pero lo intente merezco un 6, mueck</h1>
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacters} />
+        ) : (
+          <>
+          <img src={ imageRickMorty} alt="Rick & Morty" className='img-home'></img>
+          <img src=""></img>
+
+        <button onClick={reqApi} className='btn-search'>
+          Indagar
+          </button>
+          </>
+        )}
+        <ul>
+      <p>Angel Guarneros Maldonado</p>
+      <p></p>
+        <p>ZS20020316@estudiantes.uv.mx</p>
+        <p>Ingeradsilver@gmail.com</p>
+        <a href="https://github.com/BabyGuma?tab=followers" target="_blank" rel="noopener noreferrer">GitHub</a>
+      </ul>
       </header>
     </div>
   );
