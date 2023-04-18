037378e3ecff2c23b50f8dcfe4d11221de98ff8c
BabyGuma <zS20020316@estudiantes.uv.mx>
2023-04-18 07:26:00 -0600

    update

--- src/App.js
+++ src/App.js
@@ -23,7 +23,6 @@ function App() {
         ) : (
           <>
           <img src={ imageRickMorty} alt="Rick & Morty" className='img-home'></img>
-          <img src=""></img>
 
         <button onClick={reqApi} className='btn-search'>
           Indagar
